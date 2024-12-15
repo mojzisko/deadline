8d22982e75297d03558f0e3313b859c647dea066
Marek Mojžiš <mojzis9@seznam.cz>
2024-12-15 19:18:37 +0100

    fix

--- src/components/Lottie.svelte
+++ src/components/Lottie.svelte
@@ -1,11 +1,5 @@
 <script>
-  import { onMount } from "svelte";
-  import lottie from "lottie-web";
   import { DotLottieSvelte } from "@lottiefiles/dotlottie-svelte";
-  import deadline from "./DEAD_INE_json.json";
 </script>
 
 <DotLottieSvelte src="/animations/test.json" autoplay />
-
-<style>
-</style>
